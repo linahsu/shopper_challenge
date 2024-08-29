cf0fe20081fb99124f774e21bbb94ef8339c8a05
Lina Hsu <linahthsu@gmail.com>
2024-08-28 22:08:15 -0300

    feat: implements type Measures and migration

---
+++ backend/src/types/Measures.ts
@@ -0,0 +1,8 @@
+export type Measures = {
+    id: number,
+    measure_uuid: string,
+    measure_datetime: Date,
+    measure_type: string,
+    has_confirmed:boolean,
+    customer_code: string,
+}
