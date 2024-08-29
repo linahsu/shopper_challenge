6a4a9b70f3307078db0fe7354fd671ac7f8e64f8
Lina Hsu <linahthsu@gmail.com>
2024-08-29 11:51:17 -0300

    feat: create measures table

--- backend/src/database/seeders/01-measures.ts
+++ backend/src/database/seeders/01-measures.ts
@@ -8,7 +8,16 @@ export default {
         measure_datetime: '2024-08-28 14:35:00',
         measure_type: 'WATER',
         has_confirmed: true,
+        image_url: 'https://example.com/image1.jpg',
         customer_code: 12345,
+      },
+      {
+        measure_uuid: '8a6e0804-2bd0-4672-b79d-d97027f9071b',
+        measure_datetime: '2024-08-28 14:35:00',
+        measure_type: 'GAS',
+        has_confirmed: false,
+        image_url: 'https://example.com/image2.jpg',
+        customer_code: 23456,
       },
     ], {});
   },
